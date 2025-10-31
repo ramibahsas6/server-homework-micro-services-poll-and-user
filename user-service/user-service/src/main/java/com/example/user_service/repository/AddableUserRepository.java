22624a4bb14882614b175d85f57575e449e946e2
ramibahsas6 <ramibahsas6@gmail.com>
2025-10-31 18:41:20 +0200

    Delete user-service/user-service directory

--- user-service/user-service/src/main/java/com/example/user_service/repository/AddableUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.repository;
-
-import com.example.user_service.model.User;
-
-public interface AddableUserRepository {
-    User isRegisterByUserId(Long user_id);
-}
--- user-service/user-service/src/main/java/com/example/user_service/service/AddableUserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.service;
-
-import com.example.user_service.model.User;
-
-public interface AddableUserService {
-    User isRegisterByUserId(Long user_id);
-}
